1c10d4f8873ad8b2556f4351fa1be262cdf13b84
PhanHoangLocAG <phanhoangloc2017@gmail.com>
2020-10-03 09:17:05 +0700

    Chinh Sua Home and Layout

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout');
+});
+
+Route::get('/trang-chu',function(){
+    return view('layout');
 });
